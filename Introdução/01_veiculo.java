7d0990bd71cd8a66533866fd34fa16054ee25bf8
ISABELA SIMODO NAKAI <25.11733-4@maua.br>
2026-02-25 12:17:59 -0300

    Aula 1

---
+++ Introdução/01_veiculo.java
@@ -0,0 +1,54 @@
+class Casa{
+    int quantidadeJanelas;
+    int quantidadeEntradas;
+    Garagem garagem;
+
+    void abrigarPessoas(){
+
+    }
+
+    void abrirPorta(){
+
+    }
+
+    void ascenderLuz() {
+
+    }
+
+    void fecharPorta() {
+
+    }
+
+    void ligarAr() {
+        
+    }
+}
+
+class Garagem {
+    String portao;
+    String luz;
+    int vagas;
+    String entrada;
+    String saida;
+}
+
+class veiculo{
+    String chassil;
+    Motor motor;
+    String roda;
+    int quantidadePortas;
+    String assento;
+    String placa;
+
+    void estacionar() {
+
+    }
+
+    void acelerar() {
+
+    }
+}
+
+class Motor{
+    int quantidadeVelas;
+}
