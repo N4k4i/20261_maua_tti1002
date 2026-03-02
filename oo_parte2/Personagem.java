2351045e53fc4bfec6c0bdc22f258cd68d0b9257
Isabela Nakai <25.11733-4@maua.br>
2026-03-02 12:42:43 -0300

    ADD: Personagem

--- oo_parte2/Personagem.java
+++ oo_parte2/Personagem.java
@@ -1,19 +1,32 @@
 public class Personagem {
+    String habilidade;
     String nome;
-    int energia;
-    int fome;
-    int sono;
+    int energia = 10;
+    int fome = 0;
+    int sono = 0;
     //variaveis de estado 
     //métodos tem 4 partes, tipos de retorno(void), nomes(cacar), lista de parametros() e corpo{}
+    
     void cacar() {
-        System.out.println(nome + "caçando...");
+        // if energia >=2 :
+        if (energia >= 2) {
+            System.out.println(nome + " caçando...");
+            energia = energia - 2;
+        }
+        else{
+            System.out.printf("%s sem energia para caçar\n",  nome);
+        }
+    fome = fome + 1;
+    sono = sono +1;
     }
+
+
     void comer() {
         //John comendo
         System.out.printf("%s comendo\n", nome);
     }
     void dormir() {
-        System.out.print("nome + dormindo\n");
+        System.out.print(nome + " dormindo\n");
     }
 }
 
--- oo_parte2/TestePersonagem.java
+++ oo_parte2/TestePersonagem.java
@@ -1,5 +1,11 @@
 public class TestePersonagem {
     public static void main(String[] args) {
+        Personagem p1 = new Personagem();
+        p1.nome = "Enzo";
+        //fazê-lo caçar comer e dormir
+        p1.cacar();
+        p1.comer();
+        p1.dormir();
         
     }
 }
