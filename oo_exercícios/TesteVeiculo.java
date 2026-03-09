a72dea936edd1b049ef384ae9ff2159c07a5e8a1
Isabela Nakai <25.11733-4@maua.br>
2026-03-09 12:31:47 -0300

    WARN: rever

---
+++ oo_exercícios/TesteVeiculo.java
@@ -0,0 +1,6 @@
+public class TesteVeiculo {
+    //no método main
+    //construir dois veículos
+    //atribuir valores a todas as variáveisde ambos
+    //chamar os dois métodos de ambos
+}
---
+++ oo_exercícios/Veiculo.java
@@ -0,0 +1,9 @@
+public class Veiculo {
+    //adicionar tres propriedades
+
+    //encapsular as propriedades
+
+    //escrever os métodos setters para todas
+
+    //definir dois comportamnetos que um veículo
+}
